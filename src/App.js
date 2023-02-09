275948a4e7e549055e689fdcbedc9670188c7c39
Rodrigo Frenk <rfrenk@gruponxt.com>
2023-02-09 15:12:57 -0600

    importar componente en app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import SomeText from './components/example/SomeText/SomeText';
+
+
 function App() {
   return (
-    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SomeText/>
+        <SomeText text="hola"/>
+        <SomeText text="mundo"/>
       </header>
     </div>
   );
