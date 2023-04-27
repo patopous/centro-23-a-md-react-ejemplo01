900e1f03a78e143786d9ee7d9d4db3dd2c29a7e4
Rodrigo Frenk <rfrenk@gruponxt.com>
2023-04-27 17:42:04 -0600

    ejemplo cambio pantallas

--- src/components/sampleInteraction/Screens/Screens.jsx
+++ src/components/sampleInteraction/Screens/Screens.jsx
@@ -1,12 +1,58 @@
+import { useState } from "react"
+
 import style from "./Screens.module.css"
 
 const Screens = () => {
 
+    const [ screen, setScreen ] = useState( 0 )
+
+    const nextScreen = () => {
+        
+        let next = screen + 1
+        
+        if( next >= 3 ) {
+            next = 0
+        }
+
+        setScreen( next )
+
+    }
+
+    let renderScreen
+
+    switch( screen ) {
+        case 0:
+            renderScreen = <div className={ style.screen1 }>
+                <h1>Screen 1</h1>
+            </div>
+            break
+        case 1:
+            renderScreen = <div className={ style.screen2 }>
+                <h2>Screen 2</h2>
+            </div>
+            break
+        case 2:
+            renderScreen = <div className={ style.screen3 }>
+                <h3>Screen 3</h3>
+            </div>
+            break
+    }
+
     return (
         <div className={ style.Screens }>
-            screens
+            
+            { renderScreen }
+            
+            <footer className={style.Footer}>
+
+                <button onClick={nextScreen}>
+                    Next
+                </button>
+            </footer>
         </div>
     )
+
+    
 
 }
 
